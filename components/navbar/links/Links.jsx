1b8adf69565f185f4b7d80762fe3d5ba6f4df991
Ilya <stoynov.oleg@gmail.com>
2024-09-02 11:14:31 +0200

    commit
    add style and content
    - home page
    - about page

--- components/navbar/links/Links.jsx
+++ components/navbar/links/Links.jsx
@@ -1,7 +1,12 @@
+"use client"
+
 import NavLink from "./navLink/navLink"
 import styles from "./links.module.css"
+import path from "path"
+import { useState } from "react"
 
 const Links = () => {
+    
     const links = [
         {
             title: "Homepage",
@@ -19,14 +24,45 @@ const Links = () => {
             title: "Blog",
             path: "/blog"
         },
-    ]
+    ];
+
+    const [open, setOpen] = useState(false)
+
+    // Temporary
+    const session = true
+    const isAdmin = true
+
+
     return (
-    <div className={styles.links}>
-        {links.map(link => (
-            <NavLink item={link} key={link.title} />
-        ))}
+    <div className={styles.container}>
+        <div className={styles.links}>
+            {links.map(link => (
+                <NavLink item={link} key={link.title} />
+            ))}{
+            session ? (
+                <>
+                    {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}} />}
+                    <button className={styles.logout}>Logout</button>
+                </>
+            )  : (
+                <NavLink item={{title: "Login", path: "/login"}}/>
+            )  
+            }
+        </div>
+        <button className={styles.menuButton} onClick={() => setOpen(prev => !prev)}>
+            menu
+        </button>
+        {
+            open && <div className={styles.mobileLinks}>
+                {links.map(link => (
+                    <NavLink key={link.title} item={link}/>
+                ))}
+            </div>
+        }
+
     </div>
     )
 }
+
 
 export default Links
